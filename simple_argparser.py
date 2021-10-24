54a9cfb444222c92d251a2c1bfd5bfe17469913f
Benny Halevy <bhalevy@scylladb.com>
2021-10-24 10:43:42 +0300

    scylla: add simple arg parser
    For easy parsing of the api command and its args.
    
    Although https://docs.python.org/3/library/argparse.html
    provides subparsers, those has to be added in advance
    to parsing the command line args, but in our case
    we first want to allow setting the address and port
    first so we can get the api commands, and only
    then we can setup the subparsers.
    
    The simple argparser stop at '--' or the first
    unrecognized argument, placing the trailing args in
    parser.extra_args.
    
    This form the command, followed by its arguments.
    
    Signed-off-by: Benny Halevy <bhalevy@scylladb.com>

---
+++ simple_argparser.py
@@ -0,0 +1,100 @@
+"""
+Simple command line arguments parser
+"""
+
+import os
+import sys
+
+from scylla_cli import OrderedDict
+
+class ArgumentParser:
+    class Arg:
+        def __init__(self, names:list[str], dest:str, has_param=False, help:str=''):
+            self.names = names
+            self.dest = dest
+            self.has_param = has_param
+            self.help = help
+
+        def __repr__(self):
+            return f"Arg(names={self.names}, dest={self.dest}, has_param={self.has_param}, help={self.help})"
+
+    def __init__(self, description:str):
+        self.description = description
+        self.progname = os.path.basename(sys.argv[0])
+        self.args = dict()
+        self.extra_args = []
+
+        self._raw_args = OrderedDict()
+        self._by_name = dict[str, self.Arg]()
+
+        self.add_argument(['-h', '--help'], dest='help', help='show this help message and exit')
+
+    def add_argument(self, names:list[str], dest:str, has_param=False, help:str=''):
+        if type(names) is str:
+            names = [names]
+        arg = self.Arg(names, dest=dest, has_param=has_param, help=help)
+        assert len(names)
+        self._raw_args.insert(names[0], arg)
+        for n in names:
+            assert n not in self._by_name, f"arg '{n}' already added"
+            self._by_name[n] = arg
+
+    # print help message and exit
+    def usage(self):
+        s = f"Usage: {self.progname}:"
+        for arg in self._raw_args.items():
+            arg_param = f" <{arg.dest.upper()}>" if arg.has_param else ''
+            s += f" [{arg.names[0]}{arg_param}]"
+        s += f"\n\n{self.description}\n\n"
+        s += "Optional arguments:\n"
+        for arg in self._raw_args.items():
+            arg_name = arg.names[0] if len(arg.names) == 1 else '|'.join(arg.names)
+            arg_param = f" <{arg.dest.upper()}>" if arg.has_param else ''
+            justify = 21
+            arg_pfx = f"  {arg_name}{arg_param}".ljust(justify)
+            s += arg_pfx
+            if arg.help:
+                if len(arg_pfx) > justify:
+                    s += f"\n{''.ljust(justify+1)}"
+                else:
+                    s += ' '
+                s += arg.help
+            s += '\n'
+        print(f"{s}")
+        exit()
+
+    def parse_args(self, argv:list[str]=None):
+        if not argv:
+            argv = sys.argv
+        argc = 0
+        if os.path.basename(argv[0]) == self.progname:
+            argc += 1
+        while argc < len(argv):
+            opt = argv[argc]
+            if opt in self._by_name:
+                argc += 1
+                arg = self._by_name[opt]
+                if arg.has_param:
+                    if argc < len(argv):
+                        param = argv[argc]
+                        argc += 1
+                    else:
+                        print(f"Missing {arg.dest.upper()} parameter for option '{opt}'\n\n")
+                        self.usage()
+                    self.args[arg.dest] = param
+                else:
+                    self.args[arg.dest] = True
+            else:
+                if opt == '--':
+                    argc += 1
+                self.extra_args = argv[argc:]
+                break
+        if self.get('help'):
+            self.usage()
+
+    def get(self, arg:str, default_value=None):
+        try:
+            return self.args[arg]
+        except KeyError:
+            return default_value
+
